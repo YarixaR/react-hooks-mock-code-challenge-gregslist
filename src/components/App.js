eebae8505e5ae27a8a9e7cae6c6f27df587eae60
xauvk <xauvkubalexanderyang@live.com>
2022-07-26 10:30:02 -0400

    see stuff on page now

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then((response) => response.json())
+      .then((data) => setListings((listings) => data))
+      // return ()
+  }, [])
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings}) {
+  
+  
+  function displayCards () {
+    
+    const listingsArray = listings.map((item) => {
+      return (
+        <ListingCard id={item.id} description={item.description} image={item.image} location={item.location}  />
+      )
+    })
+
+    return listingsArray
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {displayCards()}
       </ul>
     </main>
   );
